5eb08b6f2492e71372c0af9b65ef01d60dcee974
NatalieOnline <137617475+NatalieOnline@users.noreply.github.com>
2023-08-25 12:46:49 +0100

    Updated button Function
    Add onclick event to button

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let passwordOne = document.getElementById("passwordone")
 let passwordtwo = document.getElementById("passwordtwo")
+let generateBtn = document.getElementById("generate")
 const characters = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9","~","`","!","@","#","$","%","^","&","*","(",")","_","-","+","=","{","[","}","]",",","|",":",";","<",">",".","?",
 "/"];
 
@@ -19,11 +20,15 @@ function generateRandomPassword() {
     return randomPassword
 }
 
+generateBtn.addEventListener("click", function() {
 let generatedPasswordOne = generateRandomPassword()
 let generatedPasswordTwo = generateRandomPassword()
 
 passwordOne.textContent = generatedPasswordOne
-passwordtwo.textContent = generatedPasswordTwo
+passwordtwo.textContent = generatedPasswordTwo   
+})
+
+
 
 
 
